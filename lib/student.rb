7155635ac87a99903d9482044a2f5798f5f9dabc
Charlie Barth <charliepbarth@gmail.com>
2020-07-27 19:57:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,14 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|k, v| self.send(("#{k}="), v)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
@@ -17,7 +20,6 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
-
